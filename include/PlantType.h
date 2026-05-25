d3ea7c16367164c6e0c70ee4376c8e7572779cda
Naomi Quinones <naomiquinones@gmail.com>
2026-05-25 02:19:43 -0700

    Modify PlantType header to match updated schema and plan

--- include/PlantType.h
+++ include/PlantType.h
@@ -2,7 +2,6 @@
 #define PLANTTYPE_H
 
 #include <string>
-using namespace std;
 
 /**
  * PlantType represents a plant species with its care requirements.
@@ -12,49 +11,67 @@ using namespace std;
 class PlantType {
 private:
     int id;
-    string latinName;
-    string family;
-    string category;
-    string origin;
-    string climate;
+    std::string latinName;
+    std::string commonNames;
+    std::string family;
+    std::string category;
+    std::string origin;
+    std::string climate;
     double tempMinC;
     double tempMaxC;
-    string idealLight;
-    string toleratedLight;
-    string watering;
-    string diseases;
+    std::string idealLight;
+    std::string toleratedLight;
+    std::string watering;
+    int wateringFrequencyDays;
+    int fertilizerFrequencyDays;
+    std::string fertilizerInstructions;
+    bool toxicToPeople;
+    bool toxicToPets;
+    std::string diseases;
 
 public:
     PlantType();
-    PlantType(int id, const string& latinName);
+    PlantType(int id, const std::string& latinName);
 
     int getId() const;
-    string getLatinName() const;
-    string getFamily() const;
-    string getCategory() const;
-    string getOrigin() const;
-    string getClimate() const;
+    std::string getLatinName() const;
+    std::string getCommonNames() const;
+    std::string getFamily() const;
+    std::string getCategory() const;
+    std::string getOrigin() const;
+    std::string getClimate() const;
     double getTempMinC() const;
     double getTempMaxC() const;
-    string getIdealLight() const;
-    string getToleratedLight() const;
-    string getWatering() const;
-    string getDiseases() const;
+    std::string getIdealLight() const;
+    std::string getToleratedLight() const;
+    std::string getWatering() const;
+    int getWateringFrequencyDays() const;
+    int getFertilizerFrequencyDays() const;
+    std::string getFertilizerInstructions() const;
+    bool isToxicToPeople() const;
+    bool isToxicToPets() const;
+    std::string getDiseases() const;
 
     void setId(int id);
-    void setLatinName(const string& name);
-    void setFamily(const string& family);
-    void setCategory(const string& category);
-    void setOrigin(const string& origin);
-    void setClimate(const string& climate);
+    void setLatinName(const std::string& name);
+    void setCommonNames(const std::string& commonNames);
+    void setFamily(const std::string& family);
+    void setCategory(const std::string& category);
+    void setOrigin(const std::string& origin);
+    void setClimate(const std::string& climate);
     void setTempMinC(double temp);
     void setTempMaxC(double temp);
-    void setIdealLight(const string& light);
-    void setToleratedLight(const string& light);
-    void setWatering(const string& watering);
-    void setDiseases(const string& diseases);
+    void setIdealLight(const std::string& light);
+    void setToleratedLight(const std::string& light);
+    void setWatering(const std::string& watering);
+    void setWateringFrequencyDays(int days);
+    void setFertilizerFrequencyDays(int days);
+    void setFertilizerInstructions(const std::string& instructions);
+    void setToxicToPeople(bool toxic);
+    void setToxicToPets(bool toxic);
+    void setDiseases(const std::string& diseases);
 
     void display() const;
 };
 
-#endif
+#endif // PLANTTYPE_H
