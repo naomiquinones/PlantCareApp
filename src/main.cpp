73e0bcf6953a1f369403e8d5e90ebbf80a5b4a3f
Naomi Quinones <naomiquinones@gmail.com>
2026-05-30 02:03:53 -0700

    Get plant input from user

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,47 @@ int main() {
             collection.displayAll();
         } else if (choice == 3) {
             // TODO: Get plant details from user
+            string commonName;
+            cout << "Enter the plant's common name: ";
+            cin >> commonName;
+
+            string scientificName;
+            cout << "Enter the plant's scientific name: ";
+            cin >> scientificName;
+
+            string lastWatered;
+            cout << "Enter the date the plant was last watered (YYYY-MM-DD). If never watered, or you don't know, leave blank: ";
+            cin >> lastWatered;
+
+            string lastFertilized;
+            cout << "Enter the date the plant was last fertilized (YYYY-MM-DD). If never fertilized, or you don't know, leave blank: ";
+            cin >> lastFertilized;
+
+            string potSize;
+            cout << "Enter the size of the pot the plant is in (Small, Medium, Large). If you don't know, leave blank: ";
+            cin >> potSize;
+
+            string rootboundStr;
+            cout << "Is the plant rootbound? (y/n): ";
+            cin >> rootboundStr;
+            bool rootbound = (rootboundStr == "y" || rootboundStr == "Y");
+
+            string notes;
+            cout << "Enter any notes about the plant: ";
+            cin >> notes;
+
+            Plant newPlant;
+            newPlant.setCommonName(commonName);
+            newPlant.setScientificName(scientificName);
+            newPlant.setLastWatered(lastWatered);
+            newPlant.setLastFertilized(lastFertilized);
+            newPlant.setPotSize(potSize);
+            newPlant.setRootbound(rootbound);
+            newPlant.setNotes(notes);
+
             // TODO: Let user select or create a plant type
+
+
             // TODO: Create Plant object and save to database
         } else if (choice == 4) {
             // TODO: Get plant name from user
